3ee9dd408702e8ceb37bf9f07ba7a802c239b22e
jareturing <59633742+jareturing@users.noreply.github.com>
2020-09-07 10:18:10 +0800

    Update run_face_main.sh

--- run_face_main.sh
+++ run_face_main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-python train_InsightFace_pallel.py  --dist-url 'tcp://172.17.51.1:23415' --dist-backend 'nccl' --multiprocess --world-size 1 --rank 0
+python train_InsightFace_pallel.py  --dist-url 'tcp://127.0.0.1:23415' --dist-backend 'nccl' --multiprocess --world-size 1 --rank 0
